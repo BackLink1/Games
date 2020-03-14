8a97f0183ce3d665f375a92ebfe7199021d9ab37
ZarQax <codercoderov@gmail.com>
2020-03-14 15:46:49 +0500

    Добавил окно и начальный обьект

--- game.py
+++ game.py
@@ -1,1 +1,11 @@
 from tkinter import * 
+
+root =Tk()
+root.geometry('800x600')
+canv = Canvas(root,bg='green')
+canv.pack(fill=BOTH,expand =1)
+x=40
+y=450
+r=20
+jumper=canv.create_oval(x-r,y-r,x+r,y+r)# krug
+mainloop()
