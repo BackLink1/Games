af283ef3fd30762a4c827c54b4c8c87828b662cf
ZarQax <codercoderov@gmail.com>
2020-03-16 14:08:30 +0500

    Added event handling

--- game.py
+++ game.py
@@ -3,20 +3,36 @@ import time
 root =Tk()
 fr = Frame(root)
 root.geometry('800x600')
-canv = Canvas(root,bg='white')
+canv = Canvas(root,bg='blue')
 canv.pack(fill=BOTH,expand =1)
-x=40
-y=450
-r=20
+
+def click(event):
+	global ay 
+	ay = -10
+	ax = 0
+canv.bind('<Left>',click) 
+x =40 
+y =45
+r =20  #radius
 jumper=canv.create_oval(x-r,y-r,x+r,y+r)# krug
-for i in range(5):
-	x+=10
+ax =6#Speed
+ay= 0
+while 1:
+	ay += 0.5
+	y += ay
+	x += ax
+	ax *= 0.99
+	if y > 550:
+		ay *= -0.7
+		ax *= 0.8
+		y=550 
 	canv.coords(jumper,x-r,y-r,x+r,y+r)
 	canv.update()
-	time.sleep(0.3)
+	time.sleep(0.03)
+	
 
 
 
 
-mainloop()
+#mainloop()
 
